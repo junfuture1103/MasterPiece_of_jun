d2b0ca6a730ba284913e587abc80cdbe856267f0
JUNTHEWORLD <62176354+junfuture1103@users.noreply.github.com>
2021-11-06 22:42:28 +0900

    Create linked list.c

---
+++ Data Structure/linked list/linked list.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#define NUM 20
+
+typedef struct node {
+	int data;
+	struct node* link;
+}node;
+
+node* new_node() {
+	struct node* temp = malloc(sizeof(struct node));
+	return temp;
+}
+
+void insert_node(node** phead, node* p, node* new_node) {
+	node* tmp = p->link;
+	p->link = new_node;
+	new_node->link = tmp;
+	if (p == *phead) {
+		(*phead)->link = new_node;
+	}
+	return;
+}
+
+int main() {
+	node* tmp = new_node();
+	node* head = tmp;
+	node* temp;
+	node* old_temp = head;
+
+	for (int i = 0; i < NUM; i++) {
+		temp = new_node();
+		temp->data = i;
+		old_temp->link = temp;
+		old_temp = temp;
+	}
+	temp->link = NULL;
+	temp = head;
+
+	for (int i = 0; i < NUM; i++) {
+		temp = temp->link;
+		printf("%d->", temp->data);
+	}
+	printf("NULL");
+
+	int input = 0;
+	//insert in normal
+	printf("\n\nGet me insert value : ");
+	if (!scanf("%d", &input)) exit(1);
+
+	//make new node
+	temp = new_node();
+	temp->data = input;
+
+	tmp = head;
+
+	while (tmp->link != NULL) {
+		tmp = tmp->link;
+		if (tmp->data >= input) break;
+	}
+
+	insert_node(&head, tmp, temp);
+	temp = head;
+
+	while (temp->link != NULL) {
+		temp = temp->link;
+		printf("%d -> ", temp->data);
+	}
+	printf("NULL");
+
+	//insert in first
+	printf("\n\nGet me insert in first value : ");
+	if (!scanf("%d", &input)) exit(1);
+
+	//make new node
+	temp = new_node();
+	temp->data = input;
+
+	tmp = head;
+	insert_node(&head, tmp, temp);
+	temp = head;
+
+	while (temp->link != NULL) {
+		temp = temp->link;
+		printf("%d -> ", temp->data);
+	}
+	printf("NULL");
+
+
+	return 0;
+}
